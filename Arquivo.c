e2c311deeed3c6efdb97485e507cc7c55ef42ab9
MARIA JULIA PELOY DE SÁ <mariajulia202007@gmail.com>
2025-11-04 10:23:21 -0300

    Update Arquivo.c
    new

--- Arquivo.c
+++ Arquivo.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main() {
-  FILE *arquivo;
-char linha  [256];
+    FILE *arquivo;
+    char linha[256];
 
-arquivo
+    arquivo = fopen("teste.txt", "rt");
+
+    if(arquivo == NULL) {
+        printf("Não foi possível ler o arquivo.\n");
+        return 1;
+    }
+
+    while (fgets(linha, sizeof(linha), arquivo) != NULL)
+    {
+        printf("%s", linha);
+    }
+    
+    fclose(arquivo);
+
+    return 0;
+}
